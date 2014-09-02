cc0c9e04b23f91a93ef5f5aee8406c674257391b
Daniel Rempel <danil.rempel@gmail.com>
2014-09-02 18:57:09 +0300

    a try to parse based on dom; rejected

--- wg.py
+++ wg.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python
+
+import sys
+import re
+from http.client import HTTPConnection
+from Article import Article
+
+class Application:
+	def __init__(self):
+		self.wikiconnection = HTTPConnection("en.wikipedia.org")
+	def checkInput(self):
+		if(len(sys.argv) < 2):
+			print("Usage: ", sys.argv[0], " <artcile name>|<article link>");
+			quit(1)
+	def parseInput(self):
+		linkPattern = re.compile("((http://)|(https://))?en\.wikipedia\.org/wiki/([A-Za-z0-9!@#$%^&*()_+=,./ -])*")
+		if(linkPattern.match(sys.argv[1])):
+			articleNamePattern = re.compile("([A-Za-z0-9!@$%^&*()_+=,. '-]*)(?:#[A-Za-z_]*)?$")
+			self.targetArticleName = articleNamePattern.findall(text)[0]
+		else:
+			self.targetArticleName = sys.argv[1]
+	
+	def run(self):
+		
+		self.checkInput()
+		self.parseInput()
+		
+		self.startArticle = Article(self.wikiconnection, self.targetArticleName)
+
+Application().run()
